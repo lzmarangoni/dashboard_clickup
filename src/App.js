df84d9cc23568af0f7b8aa6a67f457942eea21a7
Luiz Marangoni <lzmarangoni@gmail.com>
2023-05-16 16:45:51 -0300

    instalada lib apexcharts e testado com react

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Dashboard from './components/dashboard/DashBoard';
 import { Grid } from '@mui/material';
+import Chart from 'react-apexcharts'
 
 function App() {
   const getTasks = async () => {
@@ -11,6 +12,7 @@ function App() {
       .then(res => setTasks(res.data.tasks))
   }
   const [tasks, setTasks] = useState([])
+  const [tasksDate, SetTaskDate] = useState([])
   /*useEffect( ()=>{
      getTasks()
   },[])*/
@@ -20,7 +22,35 @@ function App() {
     <div className="App">
       <Grid container spacing={2}>
         <Grid item  xs={5}>
-          <Dashboard />
+        <Chart options={{
+            chart: {
+                height: 380,
+                width: 380,
+                type: "area",
+                animations: {
+                    initialAnimation: {
+                        enabled: false
+                    }
+                }
+            },
+            xaxis: {
+                type: 'datetime'
+              }
+        }} series=
+            {[
+                {
+                    name: "Series 1",
+                    data: [
+                        [1486684800000, 34],
+                        [1486771200000, 43],
+                        [1486857600000, 31],
+                        [1486944000000, 43],
+                        [1487030400000, 33],
+                        [1487116800000, 52]
+                    ]
+                }
+            ]
+            } ></Chart>
         </Grid>
       </Grid>
 
