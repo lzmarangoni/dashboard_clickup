52f3bd0d16f99260f543e722ed48f303445d27f3
Luiz Marangoni <lzmarangoni@gmail.com>
2023-05-19 16:54:53 -0300

    inclusão do tablegrid

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import Dashboard from './components/dashboard/DashBoard';
-import { Grid } from '@mui/material';
-import Chart from 'react-apexcharts'
+import { DataGrid } from '@mui/x-data-grid'
 
 function App() {
   const getTasks = async () => {
@@ -12,50 +10,42 @@ function App() {
       .then(res => setTasks(res.data.tasks))
   }
   const [tasks, setTasks] = useState([])
-  const [tasksDate, SetTaskDate] = useState([])
-  /*useEffect( ()=>{
+  const columns = [
+    {field:"id", headerName: "ID", width:"100" },
+    {field:"tarefa", headerName: "Tarefa", width:"100" },
+    {field:"projeto", headerName: "Projeto", width:"100" },
+    {field:"priority", headerName: "Priority", width:"100" },
+    {field:"tags", headerName: "Tags", width:"100" },
+    {field:"desenvolvedor", headerName: "Desenvolvedor", width:"100" },
+    {field:"situação", headerName: "Situação", width:"100" }
+  ]
+
+  const rows = [ 
+    {id:'1',tarefa:'Mudar Layout',projeto:'MUI',priority:'urgente',tags:'#bug',desenvolvedor:'Luiz',situação:'Completo'}
+  ]
+
+  useEffect( ()=>{
      getTasks()
-  },[])*/
+  },[])
+
+console.log(tasks)
 
 
   return (
     <div className="App">
-      <Grid container spacing={2}>
-        <Grid item  xs={5}>
-        <Chart options={{
-            chart: {
-                height: 380,
-                width: 380,
-                type: "area",
-                animations: {
-                    initialAnimation: {
-                        enabled: false
-                    }
-                }
+        <DataGrid
+          columns={columns}
+          rows={rows}
+          initialState={{
+            pagination: {
+              paginationModel: { page: 0, pageSize: 5 },
             },
-            xaxis: {
-                type: 'datetime'
-              }
-        }} series=
-            {[
-                {
-                    name: "Series 1",
-                    data: [
-                        [1486684800000, 34],
-                        [1486771200000, 43],
-                        [1486857600000, 31],
-                        [1486944000000, 43],
-                        [1487030400000, 33],
-                        [1487116800000, 52]
-                    ]
-                }
-            ]
-            } ></Chart>
-        </Grid>
-      </Grid>
+          }}
+          pageSizeOptions={[5, 10]}
+          checkboxSelection
 
-
-      {/*{tasks.map(task =>(<p key={task.id}>{task.name}</p>))}*/}
+/>
+      
     </div>
   );
 }
