715e7709280f2c837ec7b0a3e68e4c3a4069a28e
Luiz Marangoni <lzmarangoni@gmail.com>
2023-05-30 13:53:25 -0300

    ajustes na api para integrar com datagrid carregando todas as informações disponíveis no clickup

--- src/App.js
+++ src/App.js
@@ -3,56 +3,76 @@ import './App.css';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { DataGrid } from '@mui/x-data-grid'
+import { Login } from '@mui/icons-material';
+
+
 
 function App() {
   const getTasks = async () => {
     await axios.get("http://localhost:8000/tarefas")
-      .then(res => setTasks(res.data.tasks))
+      .then(res => setTasks(res.data)
+      
+      )
+  
   }
+
   const [tasks, setTasks] = useState([])
+  
+
   const columns = [
-    {field:"id", headerName: "ID", width:"150" },
-    {field:"name", headerName: "Tarefa", width:"400" },
-    {field:"project", headerName: "Projeto", width:"100" },
-    {field:"priority", headerName: "Priority", width:"100" },
-    {field:"tags", headerName: "Tags", width:"100" },
-    {field:"desenvolvedor", headerName: "Desenvolvedor", width:"100" },
-    {field:"situação", headerName: "Situação", width:"100" }
+    { field: "id", headerName: "ID", width: "150" },
+    { field: "name", headerName: "Tarefa", width: "300" },
+    { field: "project", headerName: "Projeto", width: "200" },
+    { field: "priority", headerName: "Priority", width: "100" },
+    { field: "tags", headerName: "Tags", width: "100" },
+    { field: "dev", headerName: "Desenvolvedor", width: "300" },
+    { field: "situacao", headerName: "Situação", width: "100" }
   ]
+ 
 
+  useEffect(() => {
+    getTasks()
+  
+  }, [])
 
-  useEffect( ()=>{
-     getTasks()
-  },[])
-
-const dataRow = tasks.map(task => {
-  return{
-    id:task.id, 
-    name:task.name,
-    project:task.project.name
+ 
+  
+  const dataRow = tasks.map(task => {
+    return(
+      {
+        id : task.id,
+        name : task.name,
+        project: task.project ? task.project.name : ('-'),
+        priority: task.priority ? task.priority.priority : ('-'),
+        tags: task.tags.map(tags => tags.name),
+        dev: task.assignees.map(devs => devs.username),
+        situacao: task.status.status
+      }
+    )
   }
-})
+   
+  )
 
-/*const rows = ()=> tasks.map(task =>
-  {'id':{{task.id}},tarefa:'Mudar Layout',projeto:'MUI',priority:'urgente',tags:'#bug',desenvolvedor:'Luiz',situação:'Completo'}
-)*/
 
+console.log(dataRow)
 
   return (
     <div className="App">
-        <DataGrid
-          columns={columns}
-          rows={dataRow}
-          initialState={{
-            pagination: {
-              paginationModel: { page: 0, pageSize: 5 },
-            },
-          }}
-          pageSizeOptions={[5, 10]}
-          checkboxSelection
-
-/>
       
+      {<DataGrid
+        sx={{m:2, border: 1}}
+        columns={columns}
+        rows={dataRow}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 10 },
+          },
+        }}
+        pageSizeOptions={[5, 10, 15, 50 ]}
+        
+
+      />}
+
     </div>
   );
 }
